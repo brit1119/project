fe452de617aa1d8c488015a26d957d0bf282daa0
Britney Tang <britneytang03@gmail.com>
2023-04-24 22:55:47 +0800

    active adjustment

--- nav.php
+++ nav.php
@@ -13,54 +13,42 @@
                 } ?>" href="index.php">Home</a></li>
 
                 <li class="dropdown"><a class="nav-link 
-                <?php if (basename($_SERVER['PHP_SELF']) == 'order_read.php') {
+                <?php if (basename($_SERVER['PHP_SELF']) == 'order_read.php' || basename($_SERVER['PHP_SELF']) == 'order_create.php') {
                     echo "active";
                 } ?>" href="order_read.php"><span>Order</span> <i class="bi bi-chevron-down"></i></a>
                     <ul>
                         <li><a href="order_read.php">My Orders</a></li>
-                        <li><a class="nav-link 
-                <?php if (basename($_SERVER['PHP_SELF']) == 'order_create.php') {
-                    echo "active";
-                } ?>" href="order_create.php">Create Order</a></li>
+                        <li><a href="order_create.php">Create Order</a></li>
                     </ul>
                 </li>
 
                 <li class="dropdown"><a class="nav-link 
-                <?php if (basename($_SERVER['PHP_SELF']) == 'product_read.php') {
+                <?php if (basename($_SERVER['PHP_SELF']) == 'product_read.php' || basename($_SERVER['PHP_SELF']) == 'product_create.php') {
                     echo "active";
                 } ?>" href="product_read.php"><span>Product</span> <i class="bi bi-chevron-down"></i></a>
                     <ul>
                         <li><a href="product_read.php">My Products</a></li>
-                        <li><a class="nav-link 
-                <?php if (basename($_SERVER['PHP_SELF']) == 'product_create.php') {
-                    echo "active";
-                } ?>" href="product_create.php">Create Product</a></li>
+                        <li><a href="product_create.php">Create Product</a></li>
                     </ul>
                 </li>
 
                 <li class="dropdown"><a class="nav-link 
-                <?php if (basename($_SERVER['PHP_SELF']) == 'category_read.php') {
+                <?php if (basename($_SERVER['PHP_SELF']) == 'category_read.php' || basename($_SERVER['PHP_SELF']) == 'category_create.php') {
                     echo "active";
                 } ?>" href="category_read.php"><span>Category</span> <i class="bi bi-chevron-down"></i></a>
                     <ul>
                         <li><a href="category_read.php">My Category</a></li>
-                        <li><a class="nav-link 
-                <?php if (basename($_SERVER['PHP_SELF']) == 'category_create.php') {
-                    echo "active";
-                } ?>" href="category_create.php">Create Category</a></li>
+                        <li><a href="category_create.php">Create Category</a></li>
                     </ul>
                 </li>
 
                 <li class="dropdown"><a class="nav-link 
-                <?php if (basename($_SERVER['PHP_SELF']) == 'customer_read.php') {
+                <?php if (basename($_SERVER['PHP_SELF']) == 'customer_read.php' || basename($_SERVER['PHP_SELF']) == 'customer_create.php') {
                     echo "active";
                 } ?>" href="customer_read.php"><span>Customer</span> <i class="bi bi-chevron-down"></i></a>
                     <ul>
                         <li><a href="customer_read.php">My Customers</a></li>
-                        <li><a class="nav-link 
-                <?php if (basename($_SERVER['PHP_SELF']) == 'customer_create.php') {
-                    echo "active";
-                } ?>" href="customer_create.php">Create Customer</a></li>
+                        <li><a href="customer_create.php">Create Customer</a></li>
                     </ul>
                 </li>
 
