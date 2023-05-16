ed0f6f6ab91372dcad4bb7fbdecdd782e7f5c303
Britney Tang <britneytang03@gmail.com>
2023-05-16 11:18:16 +0800

    edit user var

--- config/database.php
+++ config/database.php
@@ -2,11 +2,11 @@
 // used to connect to the database
 $host = "localhost";
 $db_name = "brit1119";
-$Username = "brit1119";
+$username = "brit1119";
 $password = "Xh50iAlxzo0wTP8@";
 
 try {
-    $con = new PDO("mysql:host={$host};dbname={$db_name}", $Username, $password);
+    $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
 }
 
 // show error
